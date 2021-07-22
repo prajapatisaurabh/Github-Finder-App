7e3453e76f8e8bd8cd2fa9a4139a7c016682358e
saurabhprajapati <56864338+saurabh-saurabh@users.noreply.github.com>
2021-07-22 21:27:20 +0530

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,9 +1,3 @@
 export default {
-  apiKey: "AIzaSyANxTXzNw_hBDO57PRx1W6CJlHXI4yiSEc",
-  authDomain: "githubfinder-ba39b.firebaseapp.com",
-  projectId: "githubfinder-ba39b",
-  storageBucket: "githubfinder-ba39b.appspot.com",
-  messagingSenderId: "93200889619",
-  appId: "1:93200889619:web:3056f34aa87f9b29740ce6",
-  measurementId: "G-BPDCFPTXXC",
+  # Enter Your congig
 };
